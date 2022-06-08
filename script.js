70dcead350899cdf935802f9efbd0856df027298
Egor <vaspovas@gmail.com>
2022-06-08 23:20:14 +0300

    доделал page 1 и сверстал page 2

--- script.js
+++ script.js
@@ -6,29 +6,50 @@ const blueCircle = '<img src="img/circle-blue.svg" alt="circle-blue">'
 const stepLine = document.querySelector('.step__line--1')
 const blueLine = '<img src="img/blue-line.svg" alt="line-blue">'
 const numberInput= document.querySelector('.form__num--input')
-
-function getPromptClose() {
+const stepContent2 = document.querySelector('.content__step-2')
+const errorFormInput = document.querySelector('.text__error')
+function closePrompt() {
     prompt.style.display = 'none'
 }
-promptCross.onclick = getPromptClose
+promptCross.onclick = closePrompt
 
-submit.onclick = function () {
-    getNewStepCircle()
-    getNewStepLine()
-}
+
 function getNewStepCircle() {
     circle_1.innerHTML = blueCircle
 }
 function getNewStepLine() {
     stepLine.innerHTML = blueLine
 }
+function getNewStepContent() {
+    stepContent2.style.display = 'inline'
+}
+function randomCode() {
+    alert(Math.floor(1000 + Math.random() * 9000))
+}
+
+submit.onclick = function () {
+    if (numberInput.value.length !== 10){
+        numberInput.style.borderBottomColor = 'red'
+        errorFormInput.style.display = 'flex'
+    }else{
+        getNewStepCircle()
+        getNewStepLine()
+        getNewStepContent()
+        randomCode()
+        submit.style.display = 'none'
+        errorFormInput.style.display = 'none'
+    }
 
 
-function checkPhoneNumbers() {
-    if (numberInput.value.length !== 10) {
-        numberInput.style.borderBottomColor = '#007AFF'
-    }else if (numberInput.value.length === 10){
+}
+function blurPhoneNumbers() {
         numberInput.style.borderBottomColor = '#E2E4E5'
+}
+function focusPhoneNumbers() {
+    if (numberInput.value.length !== 10) {
+       numberInput.style.borderBottomColor = '#007AFF'
     }
 }
-numberInput.onclick = checkPhoneNumbers
+numberInput.addEventListener('focus', focusPhoneNumbers)
+numberInput.addEventListener('blur', blurPhoneNumbers)
+
